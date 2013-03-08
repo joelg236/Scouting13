a24f071262e028a843debcfd5078f420a093fb0d
joelg236 <joelgallant236@gmail.com>
2013-03-07 22:01:10 -0700

    Made Ctrl-S save.

--- src/edu/ata/scouting/Scouter.java
+++ src/edu/ata/scouting/Scouter.java
@@ -6,6 +6,7 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -25,6 +26,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRootPane;
 import javax.swing.JScrollPane;
+import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 
 public final class Scouter extends JFrame {
@@ -57,6 +59,7 @@ public final class Scouter extends JFrame {
         JMenu edit = new JMenu("Edit");
 
         JMenuItem save = new JMenuItem("Save Matches");
+        save.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK));
         save.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
