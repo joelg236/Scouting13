0584d6ac05f713f27bc8ef51714f0c66eafe81b7
Joel Gallant <joelgallant236@gmail.com>
2013-03-23 23:09:41 -0600

    Working on scouting view

--- src/edu/ata/scouting/ScoutView.java
+++ src/edu/ata/scouting/ScoutView.java
@@ -1,17 +1,26 @@
 package edu.ata.scouting;
 
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+import javax.swing.JCheckBox;
 import javax.swing.JDialog;
+import javax.swing.JRootPane;
+import javax.swing.JTextArea;
 
 public final class ScoutView extends JDialog {
 
-    private final Team team;
-    private final MatchData matchData;
+    private static final Font buttonFont = new Font("Default", Font.BOLD, 18);
+    private final MatchData match;
 
-    public ScoutView(Team team, MatchData data) {
-        super(Scouter.getMain(), team + " in " + data.getMatch(), true);
-        this.team = team;
-        this.matchData = data;
+    public ScoutView(Team team, MatchData match) {
+        super(Scouter.getMain(), team + " " + match);
+        this.match = match;
+        setRootPane(new JRootPane());
+        setRootPaneCheckingEnabled(true);
+        setLayout(LayoutFactory.createLayout());
 
-        setSize(400, 400);
+        setAlwaysOnTop(true);
+        setBounds(GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds());
+        setLocationRelativeTo(null);
     }
 }
